ab8d325669514ffb995b95c3f762abd3e6d59206
Greg-C-RIsk <gregoire.paillas@c-risk.com>
2026-01-21 15:14:03 +0100

    Create VendorsView.js

---
+++ app/components/VendorsView.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default function VendorsView() {
+  return (
+    <div className="card card-pad">
+      <h2>Vendor Intake</h2>
+      <p>Define vendors and their context.</p>
+
+      <button className="btn primary">Add vendor</button>
+    </div>
+  );
+}
