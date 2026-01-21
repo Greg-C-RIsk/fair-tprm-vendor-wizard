25c573d7265febcc9a0c110c73536c7997fa65e8
Greg-C-RIsk <gregoire.paillas@c-risk.com>
2026-01-21 15:14:40 +0100

    Create ScenariosView.js

---
+++ app/components/ScenariosView.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default function ScenariosView() {
+  return (
+    <div className="card card-pad">
+      <h2>Scenario Definition</h2>
+      <p>Create specific, defensible risk scenarios.</p>
+
+      <button className="btn primary">Add scenario</button>
+    </div>
+  );
+}
