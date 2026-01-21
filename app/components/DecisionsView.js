a3e3ca05d089d1881d46513c9fa8a71c2cf64eed
Greg-C-RIsk <gregoire.paillas@c-risk.com>
2026-01-21 15:15:25 +0100

    Create DecisionsView.js

---
+++ app/components/DecisionsView.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function DecisionsView() {
+  return (
+    <div className="card card-pad">
+      <h2>Risk Decision</h2>
+      <p>Accept, mitigate, transfer or avoid based on quantified risk.</p>
+    </div>
+  );
+}
