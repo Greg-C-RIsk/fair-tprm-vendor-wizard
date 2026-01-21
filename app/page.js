36fe2356368ae56daab5c8992757beb319a1a60b
Greg-C-RIsk <gregoire.paillas@c-risk.com>
2026-01-21 17:07:19 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -204,7 +204,7 @@ export default function Page() {
       </div>
 
       {activeView === "Vendors" && selectedVendor && (
-        <VendorView
+        <VendorsView
           vendor={selectedVendor}
           addScenario={() => addScenario(selectedVendor.id)}
           updateVendor={updateVendor}
