5a8e62b0c85ff2330952775d75616c3e33efee84
Greg-C-RIsk <gregoire.paillas@c-risk.com>
2026-01-21 15:14:26 +0100

    Create TieringView.js

---
+++ app/components/TieringView.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function TieringView() {
+  return (
+    <div className="card card-pad">
+      <h2>Vendor Tiering</h2>
+      <p>Score vendors using FAIR-aligned filtering criteria.</p>
+    </div>
+  );
+}
